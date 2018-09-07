ee5591d92cc4be5a17ea598ec55be6b1c17f3f1f
Raul Burgos Murray no Taigrus <raulburgosmurray@gmail.com>
2018-09-07 17:48:05 -0400

    ejercicio 1 hecho

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -10,5 +10,16 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
 
-GOOD LUCK 😀
+GOOD LUCK 😀 
 */
+var markMass, markHeight, markBMI, johnMass, johnHeight, johnBMI = 0;
+markMass = 75;
+markHeight = 1.72;
+johnMass = 62;
+johnHeight = 1.65;
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+var markIsHigher = markBMI > johnBMI;
+console.log('Mark\'s BMI is: ' + markBMI);
+console.log('John\'s BMI is: ' + johnBMI);
+console.log('Is Mark\'s BMI higher than John\'s?: ' + markIsHigher);
